82fb3d1e29cfbb1985455adc18d489b2614d3816
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2023-11-06 21:43:17 -0300

    Estilizando Página Inicial

--- src/components/Home/PrincipalComponent.vue
+++ src/components/Home/PrincipalComponent.vue
@@ -11,8 +11,8 @@
 
       <li v-for="(item, index) in listaDeTarefas.lista" :key="index">
         {{ item }}
-        <button @click="listaDeTarefas.removerLista(index)" :disabled="!disableButton">Remover</button>
-        <button @click="editarLista(index)" :disabled="!disableButton">Editar</button>
+        <button @click="editarLista(index)" :disabled="!disableButton"></button>
+        <button  @click="listaDeTarefas.removerLista(index)" :disabled="!disableButton"></button>
       </li>
 
     </ul>
@@ -91,4 +91,52 @@ export default defineComponent({
  margin-left: 10px;
 
 }
+.listas{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.listas li{
+  background-color: #a0f46e;
+  border: none;
+  box-shadow: none;
+  border-radius: 10px;
+  transition: 0.2s;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 35px;
+  font-size: 2em;
+  margin-top: 20px;
+
+}
+.listas li:hover{
+  cursor: pointer;
+  background-color: #84ea52;
+  scale: 1.01;
+}
+.listas li button{
+  background-color: transparent;
+  border: none;
+  box-shadow: none;
+  cursor: pointer;
+  width: 25px;
+  height: 25px;
+  background-size: contain;
+  transition: 0.2s;
+  margin-left: 10px;
+ 
+}
+.listas li button:hover{
+  scale: 1.1;
+}
+.listas li button:first-child{
+  background-image: url(../../assets/editar.png);
+
+}
+.listas li button:last-child{
+  background-image: url(../../assets/lixo.png);
+}
+
+
 </style>
